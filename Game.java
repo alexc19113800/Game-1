1e5043670b78ea01d9343e74763aebadea5d9c33
alexc19113800 <alex951369@gmail.com>
2018-08-14 12:41:21 +0800

    ef

---
+++ Game.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Game{
+    public static void main(String[] args){
+        //play game
+        new Game().play();
+    }	
+
+    public Game(){
+
+    }
+
+    public void play(){
+        init();
+        run();
+    }
+
+    public void init(){
+
+    }
+
+    public void run(){
+
+    }
+}
