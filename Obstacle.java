8ff71f1d28286e0e41e10992315e21459b5ffaeb
alexc19113800 <alex951369@gmail.com>
2018-08-16 12:15:38 +0800

    obstacle

---
+++ Obstacle.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Obstacle{
+    double x,y,w,h;
+    double movB,movT;
+    boolean moveX;
+    int uOD;
+    double speed;
+    public Obstacle(double xp,double yp,double wid,double hei){
+        x=xp;y=yp;w=wid;h=hei;
+    }
+
+    public void setMovement(double bot,double top, double spd, boolean xOrY,int upOrDown){
+        movB=bot;
+        movT=top;
+        speed=spd;
+        moveX=xOrY;//true=x
+        uOD=upOrDown;
+        if(uOD>=0){uOD=1;}else{uOD=-1;}
+    }
+
+    public void move(){
+        if(moveX){
+            x+=uOD*speed;
+            if(x<=movB){
+                uOD=1;
+            }else if(x>=movT){
+                uOD=-1;
+            }
+        }else{
+            y+=uOD*speed;
+            if(y<=movB){
+                uOD=1;
+            }else if(y>=movT){
+                uOD=-1;
+            }
+        }
+    }
+}
